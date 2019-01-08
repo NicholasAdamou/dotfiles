086ea2ed53262acc9d5eba9f42606a7eaf5c64c4
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-08 16:32:05 -0500

    updated

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -7,6 +7,9 @@ declare current_dir && \
     cd "${current_dir}" && \
     source "$HOME/set-me-up/.dotfiles/utilities/utils.sh"
 
+readonly SMU_PATH="$HOME/set-me-up"
+readonly SMU_URL="https://github.com/nicholasadamou/set-me-up"
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 create_bash_local() {
@@ -89,78 +92,6 @@ create_vimrc_local() {
     else
         print_success "($FILE_PATH) already exists."
     fi
-
-}
-
-install_homebrew() {
-
-    printf "\n" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)" &> /dev/null
-	#       └─ simulate the ENTER keypress
-
-	print_result $? "Homebrew (install)" && \
-        add_brew_configs
-
-}
-
-add_brew_configs() {
-
-    declare -r BASH_CONFIGS="
-# Homebrew - The missing package manager for macOS.
-export PATH=\"/usr/local/bin:\$PATH\"
-export PATH=\"/usr/local/sbin:\$PATH\"
-"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # If needed, add the necessary configs in the
-    # local shell configuration file.
-
-    if [ ! -e "$LOCAL_BASH_CONFIG_FILE" ] || ! grep -q "$(<<<"$BASH_CONFIGS" tr '\n' '\01')" < <(less "$LOCAL_BASH_CONFIG_FILE" | tr '\n' '\01'); then
-        execute \
-            "printf '%s\n' '$BASH_CONFIGS' >> $LOCAL_BASH_CONFIG_FILE \
-                && . $LOCAL_BASH_CONFIG_FILE" \
-            "brew (update $LOCAL_BASH_CONFIG_FILE)"
-    fi
-
-}
-
-get_homebrew_git_config_file_path() {
-
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if path="$(brew --repository 2> /dev/null)/.git/config"; then
-        printf "%s" "$path"
-        return 0
-    else
-        print_error "Homebrew (get config file path)"
-        return 1
-    fi
-
-}
-
-opt_out_of_analytics() {
-
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Try to get the path of the `Homebrew` git config file.
-
-    path="$(get_homebrew_git_config_file_path)" \
-        || return 1
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Opt-out of Homebrew's analytics.
-    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
-
-    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
-        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
-    fi
-
-    print_result $? "Homebrew (opt-out of analytics)"
 
 }
 
