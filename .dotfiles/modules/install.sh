e6a7ed404e0db1d03715521cc818e93d3288ece6
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-02-07 06:54:20 -0600

    Update install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -6,6 +6,13 @@ export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"nicholasadamou/dotfiles"}
 export SMU_BLUEPRINT_BRANCH=${SMU_BLUEPRINT_BRANCH:-"master"}
 
 # The set-me-up version to download
+# Available versions:
+# 1. 'master' (MacOS)
+# 2. 'debian'
 export SMU_VERSION=${SMU_VERSION:-"master"}
 
-bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/"${SMU_VERSION}"/.dotfiles/modules/install.sh) "$@"
+# A set of ignored paths that 'git' will ignore
+# syntax: '<path>|<path>'
+export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-".dotfiles/modules/install.sh"}"
+
+bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/"${SMU_VERSION}"/.dotfiles/modules/install.sh)
