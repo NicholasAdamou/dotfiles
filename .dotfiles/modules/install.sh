400225b2b901f12d3a3d9e299fa51bc5c8537d65
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-02-07 07:04:18 -0600

    Update install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -13,6 +13,6 @@ export SMU_VERSION=${SMU_VERSION:-"debian"}
 
 # A set of ignored paths that 'git' will ignore
 # syntax: '<path>|<path>'
-export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-".dotfiles/base|.dotfiles/modules/install.sh"}"
+export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-".dotfiles/base"}"
 
 bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/"${SMU_VERSION}"/.dotfiles/modules/install.sh)
