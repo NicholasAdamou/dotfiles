f587c6fcde67e3a45471d75f1f6048404e275b57
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-02-07 06:46:04 -0600

    Update install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -9,6 +9,6 @@ export SMU_BLUEPRINT_BRANCH=${SMU_BLUEPRINT_BRANCH:-"kali-linux"}
 export SMU_VERSION=${SMU_VERSION:-"debian"}
 
 # A set of ignored paths that 'git' will ignore
-export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-".dotfiles/base .dotfiles/modules/install.sh"}"
+export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-".dotfiles/base|.dotfiles/modules/install.sh"}"
 
 bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/"${SMU_VERSION}"/.dotfiles/modules/install.sh) "$@"
