88708f86b438aa4171396fc333bfd6710d683217
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-09-02 07:02:51 -0500

    Update update.fish

--- .dotfiles/tag-kali/config/fish/functions/update.fish
+++ .dotfiles/tag-kali/config/fish/functions/update.fish
@@ -1,9 +1,11 @@
 function update --description "Updates MacOS apps, brew, npm, fisher, omf update, pip, pip3 and their installed packages"
     sudo apt update
     sudo apt upgrade -y
+    sudo apt dist-upgrade -y
     sudo apt full-upgrade -y
-    sudo apt autoremove -y
+    sudo apt autoremove -y --purge
     sudo apt clean
+    sudo apt autoclean
 
     if type -q brew
         brew update
