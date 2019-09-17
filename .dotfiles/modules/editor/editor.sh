d8ae5f40a3e7ce56796d4db22cab09d5adf5e661
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-09-17 09:39:39 -0500

    added module 'editor'

---
+++ .dotfiles/modules/editor/editor.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+
+declare current_dir && \
+    current_dir="$(dirname "${BASH_SOURCE[0]}")" && \
+    cd "${current_dir}" && \
+    source "$HOME/set-me-up/.dotfiles/utilities/utilities.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+configure_visual_studio_code() {
+
+    local extension="Shan.code-settings-sync"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! cmd_exists "code"; then
+        return 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! [ "$(code --list-extensions | grep $extension)" == "$extension" ]; then
+        execute \
+            "code --install-extension $extension" \
+            "code ($extension)"
+    else
+        print_success "($extension) is already installed"
+    fi
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "  Editor\n\n"
+
+	apt_install_from_file "packages"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	print_in_yellow "\n   Configure Visual Studio Code\n\n"
+
+    configure_visual_studio_code
+
+}
+
+main
