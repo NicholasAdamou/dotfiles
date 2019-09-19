f993b932a7af4933cc026d700d3f19e1c851eee3
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-09-19 16:34:24 -0500

    fixed a few bugs with the editor module

--- .dotfiles/modules/editor/editor.sh
+++ .dotfiles/modules/editor/editor.sh
@@ -23,7 +23,7 @@ configure_visual_studio_code() {
 
     if ! [ "$(code --list-extensions | grep $extension)" == "$extension" ]; then
         execute \
-            "code --install-extension $extension" \
+            "code --user-data-dir /root --install-extension $extension" \
             "code ($extension)"
     else
         print_success "($extension) is already installed"
