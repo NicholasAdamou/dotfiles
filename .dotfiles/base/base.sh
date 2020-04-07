1f3e4676eab979ec009f699c0523687a17c74c9d
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-07 14:32:49 -0400

    Update base.sh

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -9,18 +9,7 @@ declare current_dir && \
 
 readonly SMU_PATH="$HOME/set-me-up"
 
-declare LOCAL_BASH_CONFIG_FILE="${HOME}/.bash.local"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Overrides `utils.sh` -> print_question()
-# in order to add a few more spaces b/w '[?]'
-# & the left-most edge of the terminal window.
-print_question() {
-
-    print_in_yellow "     [?] $1"
-
-}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 create_bash_local() {
 
@@ -30,61 +19,6 @@ create_bash_local() {
 
     if [[ ! -e "$FILE_PATH" ]] || [[ -z "$FILE_PATH" ]]; then
         printf "%s\n" "#!/bin/bash" >> "$FILE_PATH"
-	fi
-
-}
-
-create_fish_local() {
-
-    declare -r FILE_PATH="$HOME/.fish.local"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if [[ ! -e "$FILE_PATH" ]] || [[ -z "$FILE_PATH" ]]; then
-        touch "$FILE_PATH"
-	fi
-
-}
-
-create_gitconfig_local() {
-
-    declare -r FILE_PATH="$HOME/.gitconfig.local"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if [[ ! -e "$FILE_PATH" ]] || [[ -z "$FILE_PATH" ]]; then
-
-        if [[ "$(git -C "$SMU_PATH" config --global --get user.name)" = "" ]] && [[ "$(git -C "$SMU_PATH" config --global --get user.email)" = "" ]]; then
-            print_in_yellow "\n   Git Configuration\n\n"
-
-            ask "What is your name? [e.g. John Smith]: "; NAME="$(get_answer)"
-            ask "What is your email address? [e.g. johnsmith@gmail.com]: "; EMAIL="$(get_answer)"
-
-            printf "\n"
-        fi
-
-        printf "%s\n" \
-"[commit]
-    # Sign commits using GPG.
-    # https://help.github.com/articles/signing-commits-using-gpg/
-    # gpgsign = true
-[user]
-    name = $NAME
-    email = $EMAIL
-    # signingkey =" \
-        >> "$FILE_PATH"
-	fi
-
-}
-
-create_vimrc_local() {
-
-    declare -r FILE_PATH="$HOME/.vimrc.local"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if [[ ! -e "$FILE_PATH" ]] || [[ -z "$FILE_PATH" ]]; then
-        touch "$FILE_PATH"
 	fi
 
 }
@@ -99,11 +33,9 @@ install_fisher() {
 
 install_fisher_packages() {
 
-	does_fishfile_exist && {
-        cat < "$HOME/.config/fish/fishfile" | while read -r PACKAGE; do
-            fisher_install "$PACKAGE"
-        done
-    }
+	cat < "$HOME/.config/fish/fishfile" | while read -r PACKAGE; do
+		fisher_install "$PACKAGE"
+	done
 
     fisher_update
 
@@ -111,15 +43,17 @@ install_fisher_packages() {
 
 symlink() {
 
+	# Get the absolute path of the .dotfiles directory.
+    # This is only for aesthetic reasons to have an absolute symlink path instead of a relative one
+    # <path-to-smu>/.dotfiles/somedotfile vs <path-to-smu>/.dotfiles/base/../somedotfile
+    readonly dotfiles="${SMU_PATH}/.dotfiles"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     # Update and/or install dotfiles. These dotfiles are stored in the .dotfiles directory.
     # rcup is used to install files from the tag-specific dotfiles directory.
     # rcup is part of rcm, a management suite for dotfiles.
     # Check https://github.com/thoughtbot/rcm for more info.
-
-    # Get the absolute path of the .dotfiles directory.
-    # This is only for aesthetic reasons to have an absolute symlink path instead of a relative one
-    # <path-to-smu>/.dotfiles/somedotfile vs <path-to-smu>/.dotfiles/base/../somedotfile
-    readonly dotfiles="${SMU_PATH}/.dotfiles"
 
     export RCRC="$dotfiles/rcrc" && \
             rcup -v -f -d "${dotfiles}"
@@ -134,20 +68,28 @@ main() {
 
 	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    create_bash_local
-	create_fish_local
-    create_gitconfig_local
-    create_vimrc_local
+    	# Install thoughtbot/RCM for dotfile management.
+    	# see: https://github.com/thoughtbot/rcm#installation
+
+    	! package_is_installed "rcm" && {
+        	wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+		echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+		sudo apt update
+		sudo apt install -y "rcm"
+    	}
 
 	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    install_fisher
+	# We must first create the $HOME/.bash.local configuration file
+	# in order for the brew module to properly install Homebrew.
+	create_bash_local
 
-    install_fisher_packages
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    symlink
+   	# We must now symlink our dotfiles prior to executing any other function.
+    	# This is required because any further action will require our dotfiles
+    	# to be present in our $HOME directory.
+    	symlink
 
 }
 
