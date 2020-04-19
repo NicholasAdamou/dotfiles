0a2f70a6f3a9c69279bb3ec679075bfd4aa59281
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-19 14:04:58 -0400

    added action comments to preferences.sh

--- .dotfiles/modules/preferences/system/ui_and_ux.sh
+++ .dotfiles/modules/preferences/system/ui_and_ux.sh
@@ -2,13 +2,18 @@
 
 # shellcheck source=/dev/null
 
+source "$HOME/set-me-up/.dotfiles/utilities/utilities.sh"
+
 declare WALLPAPER_DIRECTORY="$HOME/set-me-up/.dotfiles/modules/preferences/system/wallpaper"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+action "Change Desktop Wallpaper to $WALLPAPER_DIRECTORY/kali-red-background.jpg"
 
 xfconf-query -c xfce4-desktop \
     -p /backdrop/screen0/monitorVirtual1/workspace0/last-image \
     -s "$WALLPAPER_DIRECTORY/kali-red-background.jpg"
 
 
+action "Change hostname to 'm3m0ry'"
 sudo hostnamectl set-hostname "m3m0ry"
