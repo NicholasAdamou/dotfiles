3d1d2cee0b7769f906b645ac8f7274d79445666c
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-19 06:39:04 -0400

    rm base

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -14,6 +14,6 @@ export SMU_VERSION=${SMU_VERSION:-"debian"}
 # A set of ignored paths that 'git' will ignore
 # syntax: '<path>|<path>'
 # Note: <path> is relative to '$HOME/set-me-up'
-export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-".dotfiles/base"}"
+export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-""}"
 
 bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/"${SMU_VERSION}"/.dotfiles/modules/install.sh)
