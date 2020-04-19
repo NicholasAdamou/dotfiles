37b2f4f85c48fa0f9e54631eaa0471cb3b563271
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-19 09:53:32 -0400

    added hostnamectl to preferences.sh

--- .dotfiles/modules/preferences/system/ui_and_ux.sh
+++ .dotfiles/modules/preferences/system/ui_and_ux.sh
@@ -9,3 +9,6 @@ declare WALLPAPER_DIRECTORY="$HOME/set-me-up/.dotfiles/modules/preferences/syste
 xfconf-query -c xfce4-desktop \
     -p /backdrop/screen0/monitorVirtual1/workspace0/last-image \
     -s "$WALLPAPER_DIRECTORY/kali-red-background.jpg"
+
+
+hostnamectl set-hostname "m3m0ry"
