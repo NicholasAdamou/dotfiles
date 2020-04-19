5d34fdb4b935ebb38f37a352ce454e9b32f392c6
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-19 09:25:06 -0400

    fixed issue with ui_and_ux.sh

--- .dotfiles/modules/preferences/system/ui_and_ux.sh
+++ .dotfiles/modules/preferences/system/ui_and_ux.sh
@@ -6,12 +6,6 @@ declare WALLPAPER_DIRECTORY="$HOME/set-me-up/.dotfiles/modules/preferences/syste
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-gsettings set org.gnome.desktop.background picture-uri "file:///$WALLPAPER_DIRECTORY/kali-red-background.jpg"
-gsettings set org.gnome.desktop.background picture-options 'stretched'
-
-gsettings set org.gnome.desktop.screensaver picture-uri "file:///$WALLPAPER_DIRECTORY/kali-red-background.jpg"
-gsettings set org.gnome.desktop.screensaver picture-options 'stretched'
-
-gsettings set org.gnome.desktop.interface clock-format '12h'
-gsettings set org.gnome.desktop.interface clock-format '12h'
-gsettings set org.gnome.desktop.datetime automatic-timezone true
+xfconf-query -c xfce4-desktop \
+    -p /backdrop/screen0/monitorVirtual1/workspace0/last-image \
+    -s "$WALLPAPER_DIRECTORY/kali-red-background.jpg"
