00d5d57ffe2cfcea5d64180fc378fc36b76ff0c0
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-11-08 16:27:07 -0500

    added nord.jpg

--- .dotfiles/modules/preferences/system/ui_and_ux.sh
+++ .dotfiles/modules/preferences/system/ui_and_ux.sh
@@ -6,10 +6,10 @@ declare WALLPAPER_DIRECTORY="$HOME/set-me-up/.dotfiles/modules/preferences/syste
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-gsettings set org.gnome.desktop.background picture-uri "file:///$WALLPAPER_DIRECTORY/nord.jpeg"
+gsettings set org.gnome.desktop.background picture-uri "file:///$WALLPAPER_DIRECTORY/nord.jpg"
 gsettings set org.gnome.desktop.background picture-options 'stretched'
 
-gsettings set org.gnome.desktop.screensaver picture-uri "file:///$WALLPAPER_DIRECTORY/nord.jpeg"
+gsettings set org.gnome.desktop.screensaver picture-uri "file:///$WALLPAPER_DIRECTORY/nord.jpg"
 gsettings set org.gnome.desktop.screensaver picture-options 'stretched'
 
 gsettings set org.gnome.desktop.interface clock-format '12h'
